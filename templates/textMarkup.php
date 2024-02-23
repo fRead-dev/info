7c152fea062e9c6bf9dbd88a81fa8fc30396a3ad
SunSung W541 <mail@embedded.biz.ua>
2024-02-23 05:20:46 +0100

    * temp/filling [b648f564] :  константы для кода

---
+++ templates/textMarkup.php
@@ -0,0 +1,20 @@
+<?php
+
+trait __fRead_textMarkup{
+    const ver = "1.0";
+
+    const textAlignLeft =       ["::BL", "::EL"];
+    const textAlignRight =      ["::BR", "::ER"];
+    const textAlignCenter =     ["::BC", "::EC"];
+
+    const textStyleThroughline =    ["::BS", "::ES"];
+    const textStyleUnderline =      ["::BU", "::EU"];
+    const textStyleItalic =         ["::BI", "::EI"];
+    const textStyleBold =           ["::BB", "::EB"];
+
+    const del = [
+                    "\r",
+                    "\0",
+                    "\t"
+                  ];
+}
